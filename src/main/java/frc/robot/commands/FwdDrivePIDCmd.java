e3a0715380a5ab1228cdc4ba6fcce02956c61cad
Legion <Legion>
2022-12-16 18:20:15 -0500

    PID Loop works - Can Drive Forward

---
+++ src/main/java/frc/robot/commands/FwdDrivePIDCmd.java
@@ -0,0 +1,57 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveSub;
+
+public class FwdDrivePIDCmd extends CommandBase {
+  /** Creates a new ForwardDrivePID. */
+  private final DriveSub driveSub; 
+  
+  private double forwardValue;
+
+  private double setPoint; // Target distance
+  private double error;
+  private final double kP = 0.015625 * 1.80; // That times 32 should give 0.5 speed. 
+
+  private double outputSpeed; // This is the speed to run the motors. 
+
+  public FwdDrivePIDCmd(DriveSub driveSubsystem, double targetPoint) {
+    driveSub = driveSubsystem;
+    setPoint = targetPoint;
+    addRequirements(driveSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    driveSub.resetEncoders();
+    // setPoint += driveSub.getForwardDistance();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    forwardValue = driveSub.getForwardDistance();
+    error = setPoint - forwardValue; // Negative - negative is positive
+    outputSpeed = error * kP;  // Negative
+    driveSub.driveTank(outputSpeed, outputSpeed); //Move both at the output speed - drive forward at said speed. 
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    if(Math.abs(setPoint - driveSub.getForwardDistance()) < 6){
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
